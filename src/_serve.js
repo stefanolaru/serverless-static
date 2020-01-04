e34d9d42f53d8630e61011fa499014fad9301bb7
Stefan Olaru <contact@stefanolaru.com>
2020-01-04 08:59:58 +0200

    return 200 on OPTIONS header

--- src/_serve.js
+++ src/_serve.js
@@ -1,50 +1,54 @@
-'use strict';
-let express =  require("express")
-let morgan = require('morgan')
+"use strict";
+let express = require("express");
+let morgan = require("morgan");
 
-module.exports = (serverless, settings ) => {
-  return new Promise((resolve, reject)=>{
+module.exports = (serverless, settings) => {
+    return new Promise((resolve, reject) => {
+        // settings.folder -> folder to serve
+        // setting.port -> port used to configure localhost
 
-    // settings.folder -> folder to serve
-    // setting.port -> port used to configure localhost
+        let app = express();
 
-    let app = express()
-
-    app.use(morgan('dev', {
-      "stream": { 
-        write: (str) => { 
-            let write = `[ Static Serve ] from ${ settings.path } - ${ str }`
-            serverless.cli.log( write.replace(/[\n\r]+/g, '').trim() ) 
-        } 
-      }
-    }))
-    
-    app.use(express.static( settings.path, {
-      setHeaders:(res) => {
-        if(settings.headers.length) {
-          settings.headers.forEach(header => {
-            Object.keys(header).forEach(key => {
-              res.set(key, header[key]);
+        app.use(
+            morgan("dev", {
+                stream: {
+                    write: str => {
+                        let write = `[ Static Serve ] from ${settings.path} - ${str}`;
+                        serverless.cli.log(
+                            write.replace(/[\n\r]+/g, "").trim()
+                        );
+                    }
+                }
             })
-          });
-        }
-      }
-    }));
+        );
 
-    app.listen( settings.port, () => { 
+        app.use((req, res, next) => {
+            if (settings.headers.length) {
+                settings.headers.forEach(header => {
+                    Object.keys(header).forEach(key => {
+                        res.set(key, header[key]);
+                    });
+                });
+            }
+            if (req.method == "OPTIONS") {
+                res.send(200);
+            } else {
+                next();
+            }
+        });
 
-      serverless.cli.consoleLog('') 
-      serverless.cli.log( `[ Static ] serving files from ${ settings.path } folder` ); 
-      serverless.cli.log( `[ Static ] serving files on http://localhost:${ settings.port }` )
-      serverless.cli.consoleLog('') 
-      resolve()
+        app.use(express.static(settings.path));
 
+        app.listen(settings.port, () => {
+            serverless.cli.consoleLog("");
+            serverless.cli.log(
+                `[ Static ] serving files from ${settings.path} folder`
+            );
+            serverless.cli.log(
+                `[ Static ] serving files on http://localhost:${settings.port}`
+            );
+            serverless.cli.consoleLog("");
+            resolve();
+        });
     });
-
-  })
-}
-
-
-
-
-
+};
